b6ceb791a419e122398b15b569c54aa83773ab6c
Andres Klagges <anklagges@gmail.com>
2025-12-19 09:05:36 -0300

    Implemented new lives UI

--- Assets/02_Scripts/_Uncategorized/LivesUI.cs
+++ Assets/02_Scripts/_Uncategorized/LivesUI.cs
@@ -1,12 +1,25 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LivesUI : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _livesText;
+    [SerializeField] private Image[] _lives;
+    [SerializeField] private Color _defaultColor = Color.white;
+    [SerializeField] private Color _emptyColor = Color.black;
 
     public void SetLives(int value)
     {
-        _livesText.text = $"x{value}";
+        for (int i = 0; i < _lives.Length; i++)
+        {
+            if (i < value)
+            {
+                _lives[i].color = _defaultColor;
+            }
+            else
+            {
+                _lives[i].color = _emptyColor;
+            }
+        }
     }
 }
