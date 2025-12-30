2f6b3223d87aa1c42582799a895492852f6267e6
LAPTOP-2TCMCH4F\sirixam <maximiano.silva.a@gmail.com>
2025-12-30 13:44:54 -0300

    changes to fix the repetitive audio issue

--- Assets/02_Scripts/Interaction/Items/PaperBallController.cs
+++ Assets/02_Scripts/Interaction/Items/PaperBallController.cs
@@ -5,6 +5,11 @@ public class PaperBallController : MonoBehaviour, IInteractionOwner
     [Tooltip("Use 0 if there's no answer in this paper ball")]
     [SerializeField] private AnswerDefinition _defaultAnswerDefinition;
     [SerializeField] private ItemAudioHelper.Data _audioData;
+    [SerializeField] private AudioDefinition _throwingHitAudio;
+    [SerializeField] private float minVelocity;
+    [SerializeField] private float cooldownTime;
+    
+    private float _nextPlayTime;
 
     private ItemAudioHelper _audioHelper;
     private string _answerID;
@@ -60,10 +65,29 @@ public class PaperBallController : MonoBehaviour, IInteractionOwner
         Destroy(gameObject);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Environment") || other.CompareTag("NPC"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Environment") || collision.gameObject.CompareTag("NPC"))
         {
+            Debug.Log("Time : " + Time.time + " && nextPlayTime: " + _nextPlayTime);
+            if (Time.time <= _nextPlayTime) {
+                Debug.Log("CD is ON: " + _nextPlayTime);
+                return;
+            }
+
+            float hitMagnitude = collision.relativeVelocity.magnitude;
+
+            Debug.Log("hitMagnitude : " + hitMagnitude + " && minVelocity: " + minVelocity);
+            if (hitMagnitude <= minVelocity) {
+                Debug.Log("HitMagnitude: " + hitMagnitude);
+                return;
+            }
+                
+            _nextPlayTime = Time.time + cooldownTime;
+            Debug.Log("CD is : " + _nextPlayTime);
+            //_source.pitch = Random.Range(0.9f, 1.1f);
+            //float volume = Mathf.Clamp01(hitMagnitude / 10f);
+
             _audioHelper.OnCollide();
         }
     }
