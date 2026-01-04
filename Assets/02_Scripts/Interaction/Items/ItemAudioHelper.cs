83261ae0caed29bbe6d93339536dc30161cf76e7
Andres Klagges <anklagges@gmail.com>
2026-01-04 15:02:19 -0300

    Minor refactor to item collision audio.

--- Assets/02_Scripts/Interaction/Items/ItemAudioHelper.cs
+++ Assets/02_Scripts/Interaction/Items/ItemAudioHelper.cs
@@ -7,11 +7,14 @@ public class ItemAudioHelper
     public class Data
     {
         public AudioDefinition CollisionAudio;
+        public float MinVelocityOnCollide;
+        public float CollisionCooldown;
     }
 
     [SerializeField] private AudioDefinition _throwingHitAudio;
 
     private Data _data;
+    private float _nextPlayTime;
 
     public ItemAudioHelper(Data data)
     {
@@ -19,8 +22,29 @@ public class ItemAudioHelper
     }
 
     //hit any surface
-    public void OnCollide()
+    public void OnCollide(Collision collision)
     {
+        Debug.Log("Time : " + Time.time + " && nextPlayTime: " + _nextPlayTime);
+        if (Time.time <= _nextPlayTime)
+        {
+            Debug.Log("CD is ON: " + _nextPlayTime);
+            return;
+        }
+
+        float hitMagnitude = collision.relativeVelocity.magnitude;
+
+        Debug.Log("hitMagnitude : " + hitMagnitude + " && minVelocity: " + _data.MinVelocityOnCollide);
+        if (hitMagnitude <= _data.MinVelocityOnCollide)
+        {
+            Debug.Log("HitMagnitude: " + hitMagnitude);
+            return;
+        }
+
+        _nextPlayTime = Time.time + _data.CollisionCooldown;
+        Debug.Log("CD is : " + _nextPlayTime);
+        //_source.pitch = Random.Range(0.9f, 1.1f);
+        //float volume = Mathf.Clamp01(hitMagnitude / 10f);
+
         _data.CollisionAudio.Play();
     }
 }
--- Assets/02_Scripts/Interaction/Items/PaperBallController.cs
+++ Assets/02_Scripts/Interaction/Items/PaperBallController.cs
@@ -5,11 +5,6 @@ public class PaperBallController : MonoBehaviour, IInteractionOwner
     [Tooltip("Use 0 if there's no answer in this paper ball")]
     [SerializeField] private AnswerDefinition _defaultAnswerDefinition;
     [SerializeField] private ItemAudioHelper.Data _audioData;
-    [SerializeField] private AudioDefinition _throwingHitAudio;
-    [SerializeField] private float minVelocity;
-    [SerializeField] private float cooldownTime;
-    
-    private float _nextPlayTime;
 
     private ItemAudioHelper _audioHelper;
     private string _answerID;
@@ -69,26 +64,7 @@ public class PaperBallController : MonoBehaviour, IInteractionOwner
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Environment") || collision.gameObject.CompareTag("NPC"))
         {
-            Debug.Log("Time : " + Time.time + " && nextPlayTime: " + _nextPlayTime);
-            if (Time.time <= _nextPlayTime) {
-                Debug.Log("CD is ON: " + _nextPlayTime);
-                return;
-            }
-
-            float hitMagnitude = collision.relativeVelocity.magnitude;
-
-            Debug.Log("hitMagnitude : " + hitMagnitude + " && minVelocity: " + minVelocity);
-            if (hitMagnitude <= minVelocity) {
-                Debug.Log("HitMagnitude: " + hitMagnitude);
-                return;
-            }
-                
-            _nextPlayTime = Time.time + cooldownTime;
-            Debug.Log("CD is : " + _nextPlayTime);
-            //_source.pitch = Random.Range(0.9f, 1.1f);
-            //float volume = Mathf.Clamp01(hitMagnitude / 10f);
-
-            _audioHelper.OnCollide();
+            _audioHelper.OnCollide(collision);
         }
     }
 }
