12a83594d24ffaadfa9f866412c0e487492b3761
Claudia Provoste Pérez <claprovoste@gmail.com>
2026-01-26 21:45:02 -0300

    Commented some Logs

--- Assets/02_Scripts/Interaction/Items/ItemAudioHelper.cs
+++ Assets/02_Scripts/Interaction/Items/ItemAudioHelper.cs
@@ -24,24 +24,24 @@ public class ItemAudioHelper
     //hit any surface
     public void OnCollide(Collision collision)
     {
-        Debug.Log("Time : " + Time.time + " && nextPlayTime: " + _nextPlayTime);
+        //Debug.Log("Time : " + Time.time + " && nextPlayTime: " + _nextPlayTime);
         if (Time.time <= _nextPlayTime)
         {
-            Debug.Log("CD is ON: " + _nextPlayTime);
+            //Debug.Log("CD is ON: " + _nextPlayTime);
             return;
         }
 
         float hitMagnitude = collision.relativeVelocity.magnitude;
 
-        Debug.Log("hitMagnitude : " + hitMagnitude + " && minVelocity: " + _data.MinVelocityOnCollide);
+        //Debug.Log("hitMagnitude : " + hitMagnitude + " && minVelocity: " + _data.MinVelocityOnCollide);
         if (hitMagnitude <= _data.MinVelocityOnCollide)
         {
-            Debug.Log("HitMagnitude: " + hitMagnitude);
+            //Debug.Log("HitMagnitude: " + hitMagnitude);
             return;
         }
 
         _nextPlayTime = Time.time + _data.CollisionCooldown;
-        Debug.Log("CD is : " + _nextPlayTime);
+        //Debug.Log("CD is : " + _nextPlayTime);
         //_source.pitch = Random.Range(0.9f, 1.1f);
         //float volume = Mathf.Clamp01(hitMagnitude / 10f);
 
