5b59b4e3177ef6ddf20ffc42e4ea615733d42cd6
Sirixam <68668580+Sirixam@users.noreply.github.com>
2026-02-08 18:39:56 -0300

    changes to route with new rotations plus round timer placeholder

---
+++ Assets/02_Scripts/Time/RoundTimeHelper.cs
@@ -0,0 +1,63 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using UnityEngine;
+
+public class RoundTimeHelper
+{
+    private TimeUI _timeUI;
+
+    private bool _isPaused;
+    public bool IsRunning { get; private set; }
+    private float _remainingTime;
+
+    public Action OnTimesUp;
+
+    public RoundTimeHelper(TimeUI timeUI)
+    {
+        _timeUI = timeUI;
+    }
+
+    public void Setup(float maxTimeInSeconds)
+    {
+        _remainingTime = maxTimeInSeconds;
+        _timeUI.Setup(maxTimeInSeconds);
+    }
+
+    public async UniTask StartTimer(CancellationToken cancellationToken)
+    {
+        if (IsRunning) return;
+
+        if (_timeUI != null)
+        {
+            _timeUI.gameObject.SetActive(true);
+        }
+
+        IsRunning = true;
+        while (IsRunning && !cancellationToken.IsCancellationRequested)
+        {
+            await UniTask.Yield();
+            if (_isPaused) continue;
+
+            _remainingTime = Mathf.Max(0, _remainingTime - Time.deltaTime);
+            _timeUI.SetRemainingTime(_remainingTime);
+
+            if (_remainingTime <= 0)
+            {
+                IsRunning = false;
+                OnTimesUp.Invoke();
+            }
+        }
+        IsRunning = false;
+    }
+
+    public void Pause()
+    {
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        _isPaused = false;
+    }
+}
---
+++ Assets/02_Scripts/Time/RoundTimeUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundTimeUI : MonoBehaviour
+{
+    [SerializeField] private Image _fill;
+
+    private float _maxTimeInSeconds;
+
+    public void Setup(float seconds)
+    {
+        _fill.fillAmount = 0;
+        _maxTimeInSeconds = seconds;
+    }
+
+    public void SetRemainingTime(float seconds)
+    {
+        float percent = (_maxTimeInSeconds - seconds) / _maxTimeInSeconds;
+        _fill.fillAmount = percent;
+    }
+}
