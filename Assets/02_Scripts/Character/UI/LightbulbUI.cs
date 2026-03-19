fed82118c6a9e0fd43bc36c861c8bd0ed482c112
Andres Klagges <anklagges@gmail.com>
2026-03-19 10:47:21 -0300

    Add outline to lightbulb UI (off state). Deprecate lightbulb UI.

--- Assets/02_Scripts/Character/UI/LightbulbUI.cs
+++ Assets/02_Scripts/Character/UI/LightbulbUI.cs
@@ -1,4 +1,5 @@
-using PrimeTween;
+#define DEPRECATED // COMMENT THIS TO START USING FEATURE AGAIN
+
 using System;
 using System.Collections;
 using UnityEngine;
@@ -25,41 +26,55 @@ public class LightbulbUI : MonoBehaviour
 
     private void Awake()
     {
+#if DEPRECATED
+        gameObject.SetActive(false);
+#else
         if (_state == EState.Undefined)
         {
             SetState(_initialState == EState.On);
         }
+#endif
     }
 
     public void HideDelayed()
     {
+#if !DEPRECATED
         CancelHideCoroutine();
         _autoHideCoroutine = StartCoroutine(WaitSecondsRoutine(_defaultHideDelay, Hide));
+#endif
     }
 
     public void HideDelayed(float delay)
     {
+#if !DEPRECATED
         CancelHideCoroutine();
         _autoHideCoroutine = StartCoroutine(WaitSecondsRoutine(delay, Hide));
+#endif
     }
 
     public void Show()
     {
+#if !DEPRECATED
         CancelHideCoroutine();
         gameObject.SetActive(true);
+#endif
     }
 
     public void Hide()
     {
+#if !DEPRECATED
         CancelHideCoroutine();
         gameObject.SetActive(false);
+#endif
     }
 
     public void SetState(bool isOn)
     {
+#if !DEPRECATED
         _state = isOn ? EState.On : EState.Off;
         _onState.SetActive(isOn);
         _offState.SetActive(!isOn && _useOffState);
+#endif
     }
 
     private IEnumerator WaitSecondsRoutine(float seconds, Action callback)
